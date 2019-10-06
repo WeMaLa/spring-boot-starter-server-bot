a93ec62acf37194850e198b28e47a35a32563a09
larmic <mail@larmic.de>
2019-10-06 15:43:52 +0200

    update spring boot to 2.2.0.RC1

--- to-chat-spring-boot-autoconfiguration/src/main/kotlin/chat/to/server/bot/autoconfigure/mapper/ToChatBotPropertiesExtension.kt
+++ to-chat-spring-boot-autoconfiguration/src/main/kotlin/chat/to/server/bot/autoconfigure/mapper/ToChatBotPropertiesExtension.kt
@@ -1,10 +1,8 @@
 package chat.to.server.bot.autoconfigure.mapper
 
-import chat.to.server.bot.autoconfigure.properties.Bot
-import chat.to.server.bot.autoconfigure.properties.Server
 import chat.to.server.bot.autoconfigure.properties.ToChatBotProperties
 import chat.to.server.bot.configuration.WeMaLaConfiguration
 
 fun ToChatBotProperties.toWeMaLaConfiguration() = WeMaLaConfiguration(this.bot.toWeMaLaBot(), this.server.toWeMaLaServer())
-private fun Bot?.toWeMaLaBot() = chat.to.server.bot.configuration.Bot(this!!.identifier, this.password, this.alias)
-private fun Server?.toWeMaLaServer() = chat.to.server.bot.configuration.Server(this!!.url)
+private fun ToChatBotProperties.Bot?.toWeMaLaBot() = chat.to.server.bot.configuration.Bot(this!!.identifier, this.password, this.alias)
+private fun ToChatBotProperties.Server?.toWeMaLaServer() = chat.to.server.bot.configuration.Server(this!!.url)
--- to-chat-spring-boot-autoconfiguration/src/main/kotlin/chat/to/server/bot/autoconfigure/properties/ToChatBotProperties.kt
+++ to-chat-spring-boot-autoconfiguration/src/main/kotlin/chat/to/server/bot/autoconfigure/properties/ToChatBotProperties.kt
@@ -1,17 +1,20 @@
 package chat.to.server.bot.autoconfigure.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ImmutableConfigurationProperties
 import org.springframework.validation.annotation.Validated
 import javax.validation.constraints.NotBlank
 
 @ConfigurationProperties(prefix = "chat.to")
+@ImmutableConfigurationProperties
 @Validated
-class ToChatBotProperties(val bot: Bot, val server: Server)
+class ToChatBotProperties(val bot: Bot, val server: Server) {
 
-class Server(@NotBlank val url: String)
+    class Server(@NotBlank val url: String)
 
-class Bot(@NotBlank val identifier: String,
-          @NotBlank val password: String,
-          @NotBlank val alias: String,
-          val description: String? = null,
-          val maxCacheSize: Int = 1000)
+    class Bot(@NotBlank val identifier: String,
+              @NotBlank val password: String,
+              @NotBlank val alias: String,
+              val description: String? = null,
+              val maxCacheSize: Int = 1000)
+}
